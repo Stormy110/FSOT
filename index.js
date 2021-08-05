97189cae99f48fb2f7449958240bc9e314b2019f
Ian Storms <ianstorms110@yahoo.com>
2021-08-05 16:22:54 -0400

    added links

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const http = require('http');
+
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const es6Renderer = require('express-es6-template-engine');
+app.use(express.static('public'))
+app.engine('.html', es6Renderer);
+app.set('views', 'templates');
+app.set('view engine', 'html');
+
+
+const server = http.createServer(app);
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('/study-topics', (req, res) => {
+    res.render('study-topics');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
